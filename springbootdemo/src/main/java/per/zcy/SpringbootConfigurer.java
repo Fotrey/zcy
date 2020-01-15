041be43f2da19373106f05bfa39df157792433de
twocy <1762678583@qq.com>
2020-01-15 22:04:29 +0800

    spring boot 测试性的

---
+++ springbootdemo/src/main/java/per/zcy/SpringbootConfigurer.java
@@ -0,0 +1,11 @@
+package per.zcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfigurer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConfigurer.class);
+    }
+}
