d79cad92b6b3ba5498b182f48d93db4f0d0f92c3
twocy <1762678583@qq.com>
2020-01-27 16:23:45 +0800

    demo

---
+++ demo4study/src/main/java/pers/zcy/demo4study/Demo4studyApplication.java
@@ -0,0 +1,13 @@
+package pers.zcy.demo4study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo4studyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo4studyApplication.class, args);
+    }
+
+}
---
+++ demo4study/src/test/java/pers/zcy/demo4study/Demo4studyApplicationTests.java
@@ -0,0 +1,24 @@
+package pers.zcy.demo4study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class Demo4studyApplicationTests {
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+}
