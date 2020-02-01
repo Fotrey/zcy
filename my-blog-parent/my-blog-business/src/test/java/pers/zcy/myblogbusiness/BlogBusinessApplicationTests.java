85f6a57ca02bcc944f610597f5139e424f2d89ac
twocy <1762678583@qq.com>
2020-02-01 16:00:38 +0800

    first commit for record

---
+++ my-blog-parent/my-blog-business/src/test/java/pers/zcy/myblogbusiness/BlogBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.zcy.myblogbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ my-blog-parent/my-blog-interact/src/main/java/pers/zcy/mybloginteract/BlogInteractApplication.java
@@ -0,0 +1,13 @@
+package pers.zcy.mybloginteract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogInteractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogInteractApplication.class, args);
+    }
+
+}
